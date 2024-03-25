8c43adbb5816b2987fa442b1768c8f068bba16fc
Viacheslav <kolodiichukviacheslav@gmail.com>
2024-03-25 21:51:16 +0200

    Changes spdlog

--- lib/include/mylib/Application.hpp
+++ lib/include/mylib/Application.hpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-#include <mylib/Log.hpp>
-
 class Application {
 
 private:
--- lib/src/mylib/Application.cpp
+++ lib/src/mylib/Application.cpp
@@ -1,5 +1,7 @@
 #include <mylib/Application.hpp>
 
+#include <mylib/Log.hpp>
+
 Application::Application() {
     LOG_INFO("Start Application");
 }
